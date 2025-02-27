2f5bf64900ae845cb651e78e517428f4b3d31242
Peter Jang <peter.x.jang@gmail.com>
2025-02-27 14:12:33 -0600

    Add ability to save an image while creating a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,7 @@ class ProductsController < ApplicationController
       supplier_id: params[:supplier_id]
     )
     if @product.valid?
+      Image.create(url: params[:image_url], product_id: @product.id)
       render :show
     else
       render json: { errors: @product.errors.full_messages }, status: 422
