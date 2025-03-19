8d3615459220931821c78b28db1b85227edabec3
Peter Jang <peter.x.jang@gmail.com>
2025-03-19 12:00:53 -0500

    Add authorization for products with admin users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_18_161839) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_19_163654) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -55,5 +55,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_18_161839) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 end
