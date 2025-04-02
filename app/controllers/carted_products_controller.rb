0d7a6d3f49fb9c64b902b5fbcd603a73d8873d4d
Peter Jang <peter.x.jang@gmail.com>
2025-04-02 11:32:00 -0500

    Add carted products create action

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,2 +1,16 @@
 class CartedProductsController < ApplicationController
+  def create
+    @carted_product = CartedProduct.create(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      status: "carted",
+      order_id: nil
+    )
+    if @carted_product.valid?
+      render json: @carted_product
+    else
+      render json: { errors: @carted_product.errors.full_messages }, status: 422
+    end
+  end
 end
